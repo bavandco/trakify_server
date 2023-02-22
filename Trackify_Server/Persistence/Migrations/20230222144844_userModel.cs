4474dc51d1590abc4702b057efd115ac8646e721
Alireza <nadimzadehalireza@gmail.com>
2023-02-22 18:19:51 +0330

    User Model Set And Migrated

---
+++ Trackify_Server/Persistence/Migrations/20230222144844_userModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class userModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "F_Name",
+                table: "AspNetUsers",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FristName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleAuthCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FristName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GoogleAuthCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "F_Name");
+        }
+    }
+}
