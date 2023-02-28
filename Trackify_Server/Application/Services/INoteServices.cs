09af6182ee9bb7258cd76cef59571bb8277a8d9d
Alireza <nadimzadehalireza@gmail.com>
2023-02-28 09:40:28 +0330

    Note services in place

--- Trackify_Server/Application/Services/INoteServices.cs
+++ Trackify_Server/Application/Services/INoteServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Application.Services
     public interface INoteServices
     {
         public void CreateNote(string text, string userId, int happiness, int satisfaction, int health);
-        public void DeleteNote(string signedInUserId, Guid noteId);
+        public int DeleteNote(string signedInUserId, Guid noteId);
         public void UpdateNote(string userId,Guid id, string text, int happiness, int satisfaction, int health);
         public GetNoteDto GetNote(string signedInUserId,Guid noteId);
         public List<GetNoteDto> GetAllUserNotes(string signedInUserId,string targetUserId,int pageNumber,int pageSize);
@@ -17,7 +18,109 @@ namespace Application.Services
     }
     public class NoteServices : INoteServices
     {
+        NoteRepository _noteRepo;
 
+        public NoteServices(NoteRepository _noteRepo)
+        {
+            this._noteRepo = _noteRepo;
+        }
+        public void CreateNote(string text, string userId, int happiness, int satisfaction, int health)
+        {
+            _noteRepo.CreateNote(text, userId, happiness, satisfaction, health);
+        }
+
+        public int DeleteNote(string signedInUserId, Guid noteId)
+        {
+            var note = _noteRepo.GetNote(noteId);
+            if (note.UserId == signedInUserId)
+            {
+                _noteRepo.RemoveNote(noteId);
+                return 0;
+            }
+            return 1;
+            
+        }
+
+        public List<GetNoteDto> GetAllUserNotes(string signedInUserId, string targetUserId, int pageNumber, int pageSize)
+        {
+            List<GetNoteDto> result = new List<GetNoteDto>();
+            if(pageNumber >= 1 && signedInUserId==targetUserId)
+            {
+                var notes = _noteRepo.GetAllUserNotes(signedInUserId, pageNumber, pageSize);
+                foreach (var note in notes)
+                {
+                    result.Add(new GetNoteDto()
+                    {
+                        NoteId = note.Id,
+                        UserId = note.UserId,
+                        CreatedDate = note.CreatedAt,
+                        Happiness = note.Happiness,
+                        Health = note.Health,
+                        Satisfaction = note.Satisfaction,
+                        Text = note.Text,
+                    });
+                }
+                return result;
+            }else
+            {
+                return null;
+            }
+            
+        }
+
+        public GetNoteDto GetNote(string signedInUserId, Guid noteId)
+        {
+            var note = _noteRepo.GetNote(noteId);
+            if (note.UserId == signedInUserId)
+            {
+                return new GetNoteDto()
+                {
+                    UserId = note.UserId,
+                    Text = note.Text,
+                    Satisfaction = note.Satisfaction,
+                    CreatedDate = note.CreatedAt,
+                    Happiness= note.Happiness,
+                    Health= note.Health,
+                    NoteId = note.Id,
+                };
+            }
+            return null;
+        }
+
+        public List<GetNoteDto> GetNotesBasedOnDateRange(string signedInUserId, string targetUserId, DateTime startingDate, DateTime endingDate, int pageNumber, int pageSize)
+        {
+            List<GetNoteDto> result = new List<GetNoteDto>();
+            if (pageNumber >= 1 && signedInUserId == targetUserId)
+            {
+                var notes =_noteRepo.GetNotesBasedOnDateRange(signedInUserId, startingDate, endingDate, pageNumber, pageSize);
+                foreach (var note in notes)
+                {
+                    result.Add(new GetNoteDto()
+                    {
+                        NoteId = note.Id,
+                        UserId = note.UserId,
+                        CreatedDate = note.CreatedAt,
+                        Happiness = note.Happiness,
+                        Health = note.Health,
+                        Satisfaction = note.Satisfaction,
+                        Text = note.Text,
+                    });
+                }
+                return result;
+            }
+            return null;
+            
+        }
+
+        public void UpdateNote(string userId, Guid id, string text, int happiness, int satisfaction, int health)
+        {
+            var note = _noteRepo.GetNote(id);
+            if(note.UserId == userId)
+            {
+                _noteRepo.UpdateNote(id, text, happiness, satisfaction, health);
+            }
+            
+        }
     }
 
     public class GetNoteDto
@@ -25,9 +128,9 @@ namespace Application.Services
         public Guid NoteId;
         public string Text;
         public string UserId;
-        public int Happiness;
-        public int Satisfaction;
-        public int Health;
+        public int? Happiness;
+        public int? Satisfaction;
+        public int? Health;
         public DateTime CreatedDate;
     }
 }
