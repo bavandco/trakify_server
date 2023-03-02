ed159d1bf60aeb846251940fe4320272974a5ceb
Alireza <nadimzadehalireza@gmail.com>
2023-03-02 18:25:52 +0330

    Basic JWT configuration

---
+++ Trackify_Server/Application/Services/ITokenValidator.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Models.Users;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface ITokenValidator
+    {
+        Task Execute(TokenValidatedContext context);
+    }
+
+    public class TokenValidate : ITokenValidator
+    {
+        private readonly UserManager<User> _userManager;
+        public TokenValidate(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task Execute(TokenValidatedContext context)
+        {
+            var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+            if(claimsIdentity == null || !claimsIdentity.Claims.Any())
+            {
+                context.Fail("claims not found");
+                return;
+            }
+            var userId = claimsIdentity.FindFirst("UserId").Value;
+            if(!Guid.TryParse(userId,out Guid userGuid))
+            {
+                context.Fail("claims not found");
+                return;
+            }
+
+        }
+    }
+}
--- Trackify_Server/MainEndpoint/Extensions/ServiceExtensions.cs
+++ Trackify_Server/MainEndpoint/Extensions/ServiceExtensions.cs
@@ -1,4 +1,9 @@
-﻿namespace MainEndpoint.Extensions
+﻿using Application.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MainEndpoint.Extensions
 {
     public static class ServiceExtensions
     {
@@ -16,5 +21,42 @@
             {
 
             });
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration
+configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = Environment.GetEnvironmentVariable("SECRET");
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["validIssuer"],
+                    ValidAudience = jwtSettings["validAudience"],
+
+                    IssuerSigningKey = new
+    SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+                options.SaveToken = true;
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var tokenValidatorService = context.HttpContext.RequestServices.GetRequiredService<ITokenValidator>();
+
+                        return tokenValidatorService.Execute(context);
+                    }
+                };
+            });
+        }
     }
 }
