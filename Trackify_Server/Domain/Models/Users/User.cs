adb2bbad4d30954da437989b39b79cf0447dd17e
Alireza <nadimzadehalireza@gmail.com>
2023-03-05 22:19:53 +0330

    Seperated UserToken from User

--- Trackify_Server/Domain/Models/Users/User.cs
+++ Trackify_Server/Domain/Models/Users/User.cs
@@ -22,14 +22,4 @@ namespace Domain.Models.Users
         FEMALE,
         OTHER
     }
-
-    public class UserToken
-    {
-        public int Id { get; set; }
-        public string TokenHash { get; set; }
-        public DateTime TokenExp { get; set; }
-        public string UserId { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime RefreshTokenExp { get; set; }
-    }
 }
---
+++ Trackify_Server/Domain/Models/Users/UserToken.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models.Users
+{
+    public class UserToken
+    {
+        public int Id { get; set; }
+        public string TokenHash { get; set; }
+        public DateTime TokenExp { get; set; }
+        public string UserId { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExp { get; set; }
+    }
+}
