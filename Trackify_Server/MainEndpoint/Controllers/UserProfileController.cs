961c6c506e97ef691168971fcd7620ed0e199590
Alireza <nadimzadehalireza@gmail.com>
2023-03-09 12:33:42 +0330

    UserProfileController and UploadUserPrrofilePicture in place fixed typo in program.cs

---
+++ Trackify_Server/MainEndpoint/Controllers/UserProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MainEndpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserServices _userServices;
+        public UserProfileController(IUserServices _userServices)
+        {
+            this._userServices = _userServices;
+        }
+        [HttpPost]
+        [Route("UploadUserProfilePicture")]
+        public async Task<string> UploadUserProfilePicture([FromForm] IFormFile file)
+        {
+            string userId = User.Claims.First(x => x.Type == "UserId").Value;
+            return _userServices.CreateUserProfilePicture(userId, file);
+
+        }
+    }
+}
