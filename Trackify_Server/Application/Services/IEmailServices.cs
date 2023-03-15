77f8583267760c4b60f407b06176ff561f7d84ff
Alireza <nadimzadehalireza@gmail.com>
2023-03-15 22:17:22 +0330

    EmailService done

---
+++ Trackify_Server/Application/Services/IEmailServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class EmailService
+    {
+        public Task Execute(string UserEmail, string Body, string Subject)
+        {
+            //enable less secure apps in account google with link
+            //https://myaccount.google.com/lesssecureapps
+
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 1000000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("Trackify-Email", "Trackify-Email-Password");
+            MailMessage message = new MailMessage("Trackify-Email", UserEmail, Subject, Body);
+            message.IsBodyHtml = true;
+            message.BodyEncoding = UTF8Encoding.UTF8;
+            message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
+            client.Send(message);
+            return Task.CompletedTask;
+        }
+    }
+
+
+
+}
